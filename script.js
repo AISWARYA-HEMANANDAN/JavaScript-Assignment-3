cc7d796abe7a17d3ac9c4c64f204b1887662d9e2
Aiswarya K T <kt.aiswarya@gmail.com>
2024-11-11 17:30:03 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1. Using getElementById
+
+let element = document.getElementById("heading")
+console.log(element);
+
+
+// 2. Using getElementsByClassName
+
+// let element = document.getElementsByClassName("container")
+// console.log(element);
+
+// let element = document.getElementsByClassName("section")
+// console.log(element);
+
+
+// 3. Using getElementsByTagName
+
+// let element = document.getElementsByTagName("p")
+// console.log(element);
+
+
+// 4. Using querySelector
+
+// let element = document.querySelector(".btn")
+// console.log(element);
+
+
+// 5. Using querySelectorAll
+
+// let element = document.querySelectorAll(".btn")
+// console.log(element);
+
+// let element = document.querySelectorAll("p")
+// console.log(element);
